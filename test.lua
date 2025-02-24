3e7630a04a7d7f06e9ddb394dd67e8fd41462262
SkibidiDashhi <voonyhongiv23@gmail.com>
2025-02-24 22:10:08 +0630

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,44 +1,143 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua"))()
+--------------------------------------------------------------------------------------------------------------------------------------------
+--Place Id Check
+local id = game.PlaceId
+if id == 2753915549 then First_Sea = true; elseif id == 4442272183 then Second_Sea = true; elseif id == 7449423635 then Third_Sea = true; else game:Shutdown() end;
+--------------------------------------------------------------------------------------------------------------------------------------------
+--Flag Player
+function AntiBan()
+    for i,v in pairs(game:GetService("Players").LocalPlayer.Character:GetDescendants()) do
+        if v:IsA("LocalScript") then
+            if v.Name == "General" or v.Name == "Shiftlock"  or v.Name == "FallDamage" or v.Name == "4444" or v.Name == "CamBob" or v.Name == "JumpCD" or v.Name == "Looking" or v.Name == "Run" then
+                v:Destroy()
+            end
+        end
+     end
+     for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerScripts:GetDescendants()) do
+        if v:IsA("LocalScript") then
+            if v.Name == "RobloxMotor6DBugFix" or v.Name == "Clans"  or v.Name == "Codes" or v.Name == "CustomForceField" or v.Name == "MenuBloodSp"  or v.Name == "PlayerList" then
+                v:Destroy()
+            end
+        end
+     end
+    end
+    AntiBan()
+--------------------------------------------------------------------------------------------------------------------------------------------
+--Anti AFK
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+	game:GetService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+	wait(1)
+	game:GetService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
 
-local Window = MakeWindow({
-    Hub = {
-        Title = "Random Hub",
-        Animation = "By: Dashh"
-    },
-    Key = {
+    local plr = game.Players.LocalPlayer
+	local CbFw = getupvalues(require(plr.PlayerScripts.CombatFramework))
+	local CbFw2 = CbFw[2]
+
+    function GetCurrentBlade() 
+        local p13 = CbFw2.activeController
+        local ret = p13.blades[1]
+        if not ret then return end
+        while ret.Parent~=game.Players.LocalPlayer.Character do ret=ret.Parent end
+        return ret
+    end
+    
+    function AttackNoCD()
+        if not AutoFarmMasDevilFruit or AutoFarmMasGun then
+            if not Auto_Raid then
+                local AC = CbFw2.activeController
+                for i = 1, 1 do 
+                    local bladehit = require(game.ReplicatedStorage.CombatFramework.RigLib).getBladeHits(
+                        plr.Character,
+                        {plr.Character.HumanoidRootPart},
+                        60
+                    )
+                    local cac = {}
+                    local hash = {}
+                    for k, v in pairs(bladehit) do
+                        if v.Parent:FindFirstChild("HumanoidRootPart") and not hash[v.Parent] then
+                            table.insert(cac, v.Parent.HumanoidRootPart)
+                            hash[v.Parent] = true
+                        end
+                    end
+                    bladehit = cac
+                    if #bladehit > 0 then
+                        local u8 = debug.getupvalue(AC.attack, 5)
+                        local u9 = debug.getupvalue(AC.attack, 6)
+                        local u7 = debug.getupvalue(AC.attack, 4)
+                        local u10 = debug.getupvalue(AC.attack, 7)
+                        local u12 = (u8 * 798405 + u7 * 727595) % u9
+                        local u13 = u7 * 798405
+                        (function()
+                            u12 = (u12 * u9 + u13) % 1099511627776
+                            u8 = math.floor(u12 / u9)
+                            u7 = u12 - u8 * u9
+                        end)()
+                        u10 = u10 + 1
+                        debug.setupvalue(AC.attack, 5, u8)
+                        debug.setupvalue(AC.attack, 6, u9)
+                        debug.setupvalue(AC.attack, 4, u7)
+                        debug.setupvalue(AC.attack, 7, u10)
+                        pcall(function()
+                            if plr.Character:FindFirstChildOfClass("Tool") and AC.blades and AC.blades[1] then
+                                AC.animator.anims.basic[1]:Play(0.01,0.01,0.01)
+                                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("weaponChange",tostring(GetCurrentBlade()))
+                                game.ReplicatedStorage.Remotes.Validator:FireServer(math.floor(u12 / 1099511627776 * 16777215), u10)
+                                game:GetService("ReplicatedStorage").RigControllerEvent:FireServer("hit", bladehit, i, "")
+                            end
+                        end)
+                    end
+                end
+            end
+        end
+    end
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V2/refs/heads/main/UiREDzV2.lua")))()
+
+       local Window = MakeWindow({
+         Hub = {
+         Title = "Random Hub",
+         Animation = "By: Dashh"
+         },
+        Key = {
         KeySystem = false,
         Title = "Get Key",
-        Description = "Random Hub Auto Hop (FREE)",
+        Description = "Teddy Hub Auto Hop (FREE)",
         KeyLink = "https://link4m.com/21cId",
         Keys = {"teddyseetinkvaicut"},
         Notifi = {
-            Notifications = true,
-            CorrectKey = "Đang chạy script...",
-            Incorrectkey = "Key sai",
-            CopyKeyLink = "Đã sao chép"
-        }
+        Notifications = true,
+        CorrectKey = "Đang chạy script...",
+       Incorrectkey = "Key sai",
+       CopyKeyLink = "Đã sao chép"
+      }
     }
-})
+  })
 
-MinimizeButton({
-    Image = "http://www.roblox.com/asset/?id=76190632660322",
-    Size = {50, 50},
-    Color = Color3.fromRGB(10, 10, 10),
-    Corner = true,
-    Stroke = false,
-    StrokeColor = Color3.fromRGB(255, 0, 0)
-})
+       MinimizeButton({
+       Image = "http://www.roblox.com/asset/?id=76190632660322",
+       Size = {50, 50},
+       Color = Color3.fromRGB(10, 10, 10),
+       Corner = true,
+       Stroke = false,
+       StrokeColor = Color3.fromRGB(255, 0, 0)
+      })
+      
+------ Tab
+     local Tab1o = MakeTab({Name = "Main"})
+     local Tab2o = MakeTab({Name = "Settings"})
+     
+     
+-------TOGGLE 
 
-local Tab1o = MakeTab({Name = "Main"})
-local Tab2o = MakeTab({Name = "Settings"})
-
-Toggle = AddToggle(Tab1o, {
-    Name = "Auto Kill RipIndra(wait update)",
-    Default = false,
-    Callback = function(value)
-    end
-})
-
+     Toggle = AddToggle(Tab1o, {
+      Name = "Auto Kill RipIndra",
+      Default = false,
+      Callback = function()
+     end
+    })
+    
+------- BUTTON
+    
     AddButton(Tab1o, {
      Name = "Auto Hop Rip_indra",
     Callback = function()
@@ -46,42 +145,18 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnBeoDepT
 end
 })
 
-Toggle = AddToggle(Tab1o, {
-    Name = "Auto Kill DoughKing(wait update)",
-    Default = false,
-    Callback = function(value)
-    end
-})
+-------TOGGLE 
 
+     Toggle = AddToggle(Tab1o, {
+      Name = "Auto Kill DoughKing",
+      Default = false,
+      Callback = function()
+     end
+    })
+    
    AddButton(Tab1o, {
      Name = "Auto Hop Dough King ",
     Callback = function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AnDepZaiHub/AnBeoDepTrai/refs/heads/main/Dough_King"))()
 end
-})
-
-Dropdown = AddDropdown(Tab2o, {
-    Name = "Select Tesst",
-    Options = {"Melee", "Sword", "Blox Fruit"},
-    Default = "Melee",
-    Callback = function(value)
-    end
-})
-
-Toggle = AddToggle(Tab2o, {
-    Name = "Auto Turn On Buso",
-    Default = true,
-    Callback = function(value)
-        local function AutoHaki()
-            if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("HasBuso") then
-                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Buso")
-            end
-        end
-
-        if value then
-            while wait() and value do
-                AutoHaki()
-            end
-        end
-    end
 })
